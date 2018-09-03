9aa73a0202646c6434a804e6aecd1c6ed825738e
tfl xin <1445971703@qq.com>
2018-09-03 13:57:19 +0800

    自定义的一个试图类ViewObject 是一个map包装类，方便吧每一条咨询的信息（包括  新闻信息，发帖的用户）放到一起，传到moder，方便前端拿到，做展示

--- src/main/java/com/nowcoder/model/ViewObject.java
+++ src/main/java/com/nowcoder/model/ViewObject.java
@@ -6,6 +6,9 @@ import java.util.Objects;
 
 /**
  * Created by rainday on 2018/6/30.
+ 自定义的一个试图类ViewObject 是一个map包装类，方便吧每一条咨询的信息（包括
+ 新闻信息，发帖的用户）放到一起，传到moder，方便前端拿到，做展示
+
  */
 public class ViewObject {
     //专门用来试视图展示
