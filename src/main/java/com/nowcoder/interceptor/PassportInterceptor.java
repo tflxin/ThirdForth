63bf0902f2fd27a3b6823762ade8d42a70424ba9
tfl xin <1445971703@qq.com>
2018-09-03 16:28:27 +0800

    权限验证：PassportInterceptor

--- src/main/java/com/nowcoder/interceptor/PassportInterceptor.java
+++ src/main/java/com/nowcoder/interceptor/PassportInterceptor.java
@@ -17,10 +17,19 @@ import java.util.Date;
 
 /**
  * Created by nowcoder on 2016/8/3.
+
  访问首页的时候，最先访问的是拦截器，此时controller还没有进来
+ 对于登录，注册成功后会自动登录:登录操作中，在service层进行逻辑判断，向上返回到controller，乡下Dao去和数据库交互
+ 登录代码中：服务器会生成 string类型的ticket，存入到cookies中，key值是ticket。value是ticket的值用过response下发到服务器
+
+ 权限验证：PassportInterceptor
  先访问preHandle：遍历cookie，找到ticket对应的值（浏览器缓存的那个ticket登录）
- 然后去数据库里面找这个ticket，判断信息是否有效，如有效，
- 去userDAO里面通过ticket找到这个用户
+                 然后去数据库里面找这个ticket，判断信息是否有效，如有效，
+                  去userDAO里面通过ticket找到这个用户，
+                  把登陆的用户放到线程本地变量中，此时才进入controller
+ postHandle:
+
+ https://blog.csdn.net/chenchunlin526/article/details/69939337
  @Component 把普通pojo实例化到spring容器中，相当于配置文件中的<bean id="" class=""/>
  定义Spring管理Bean
  */
@@ -36,6 +45,16 @@ public class PassportInterceptor implements HandlerInterceptor {
     @Autowired
     private HostHolder hostHolder;
 
+    /**
+     *
+     * @param httpServletRequest
+     * @param httpServletResponse
+     * @param o
+     * @return
+     * @throws Exception
+     * 通过passportInteger的preHandler方法之后，进入到LoginRequiresInterceptor的
+     * PreHandler方法，如果没有拿到user类，返回false不能进入controller类
+     */
     @Override
     public boolean preHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o) throws Exception {
         String ticket = null;
@@ -59,8 +78,17 @@ public class PassportInterceptor implements HandlerInterceptor {
             //保存该用户，按照依赖注入的方法，因为以后很多地方都可能调用这个当前用户
             hostHolder.setUser(user);//存在线程里面threadLocal
         }
-        return true;
+        return true;//意味着允许进入controller层
     }
+
+    /**
+     *
+     * @param httpServletRequest
+     * @param httpServletResponse
+     * @param o
+     * @param modelAndView
+     * @throws Exception
+     */
 
     @Override
     public void postHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, ModelAndView modelAndView) throws Exception {
